d5db60135c31af4a06f186d6d2ede42bf1aa5a66
gcros <guilhem.cros.38@gmail.com>
2024-08-13 16:51:29 +0200

    update forbidden function

--- config/forbidden.mk
+++ config/forbidden.mk
@@ -1,4 +1,4 @@
-FFUNCTION = readline|rl_clear_history|rl_on_new_line|rl_replace_line|rl_redisplay|add_history|printf|malloc|free|write|access|open|read|close|fork|wait|waitpid|wait3|wait4|signal|sigaction|sigemptyset|sigaddset|kill|exit|getcwd|chdir|stat|lstat|fstat|unlink|execve|dup|dup2|pipe|opendir|readdir|closedir|strerror|perror|isatty|ttyname|ttyslot|ioctl|getenv|tcsetattr|tcgetattr|tgetent|tgetflag|tgetnum|tgetstr|tgoto|tputs
+FFUNCTION = open|close|read|write|printf|malloc|free|perror|strerror|exit
 
 .PHONY: check_forbidden_function
 check_forbidden_function:
