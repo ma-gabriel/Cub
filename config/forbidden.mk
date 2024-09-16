c8a7a3d80867655c72f135a33e790d51c1331629
ma-gabriel <gabi.eymat@gmail.com>
2024-09-16 10:59:45 +0200

    test ?

--- config/forbidden.mk
+++
@@ -1,7 +0,0 @@
-FFUNCTION = open|close|read|write|printf|malloc|free|perror|strerror|exit
-
-.PHONY: check_forbidden_function
-check_forbidden_function:
-	@echo "forbidden function"
-	@nm -u $(NAME) | egrep -v -w "$(FFUNCTION)"
-
