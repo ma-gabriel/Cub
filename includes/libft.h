a8fd772566ab9105ca6800bd53d3283b6072e91b
gcros <guilhem.cros.38@gmail.com>
2024-09-20 15:19:03 +0200

    add ft_memswap

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 16:14:33 by gcros             #+#    #+#             */
-/*   Updated: 2024/08/13 16:15:56 by gcros            ###   ########.fr       */
+/*   Updated: 2024/09/17 22:05:32 by gcros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,8 @@
 # include "put.h"
 # include "arr.h"
 # include "vector.h"
+
+void	ft_memswap(void *p1, void *p2, size_t len);
+void	ft_memswapbyte(void *p1, void *p2, size_t len);
 
 #endif
