0bc43461a342a5ca618006a7e002aae85508d9d1
ma-gabriel <gabi.eymat@gmail.com>
2024-10-21 13:37:46 +0200

    hey

--- srcs/cub_manip/cm_put_line.c
+++ srcs/cub_manip/cm_put_line.c
@@ -15,49 +15,20 @@
 #include "libft.h"
 #include "stdio.h"
 #include "draw.h"
-/*
-void	cm_put_line(t_img_p img, t_rc_event_p rc, int x)
+
+void	cm_put_line(t_img_p canvas, t_rc_event_p rc, int canvas_x)
 {
-	const t_img_p	rimg = rc->img;
-	const int		o = rc->offset * rimg->width;
-	const double	p = rimg->height / (double)img->height;
+	const int		layer_x = rc->offset * rc->img->width;
+	const int		beginning = (canvas->height + canvas->height / rc->dist) / 2;
+	const int		limit = ft_min(canvas->height, beginning);
+	const double	ratio = (double) (rc->img->height) * rc->dist / (double) canvas->height;
 	int				i;
 
-	const int y = img->height / 2; // la valeur ou i est a la moitie
-
-	rc->dist = 2;
-	i = img->height / (double) (rc->dist * 4) + y ;
-	const int temp = i ; // la valeur ou i doit s'arreter
-	while (i-- > -temp + 2 * y)
+	i = canvas->height - limit;
+	while (i < limit)
 	{
-		mm_img_putpixel_s(img, x, i,
-			mm_img_getpixel_s(rimg, o, i * p));
-	}
-} */
-
-void	cm_put_line(t_img_p img, t_rc_event_p rc, int x)
-{
-	const t_img_p	rimg = rc->img;
-	const int		o = rc->offset * rimg->width;
-
-	const int	half = img->height >> 1; // la valeur ou i est a la moitie
-	int	high = ((int) (half / (double) rc->dist) >> 1) - 1.;
-	const double	p = 1. / (double)(half);
-	int				i;
-
-	i = high + half;
-	double	ip = (i * p - 1. + high * p) * rimg->height * (double) rc->dist;
-	double		remove = p * (double) rimg->height * rc->dist;
-	if (high > half)
-	{
-		i = img->height;
-		ip = (i * p - 1. + high * p) * rimg->height * (double) rc->dist;
-		high = half;
-	}
-	while (i-- > half - high)
-	{
-		mm_img_putpixel_s(img, x, i,
-			mm_img_getpixel_s(rimg, o, ip));
-		ip -= remove;
+		mm_img_putpixel(canvas, canvas_x, i,
+			mm_img_getpixel(rc->img, layer_x, (i + beginning - canvas->height) * ratio));
+		i++;
 	}
 }
