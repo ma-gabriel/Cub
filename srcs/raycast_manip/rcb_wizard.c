391eb649a43d7436129e50a2dc0e169cb99a1980
gcros <guilhem.cros.38@gmail.com>
2024-10-24 17:00:51 +0200

    .

--- srcs/raycast_manip/rcb_wizard.c
+++ srcs/raycast_manip/rcb_wizard.c
@@ -6,7 +6,7 @@
 /*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 21:01:26 by gcros             #+#    #+#             */
-/*   Updated: 2024/10/24 16:43:50 by gcros            ###   ########.fr       */
+/*   Updated: 2024/10/24 16:56:11 by gcros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <stdio.h>
 #include <math.h>
 #include "texture_manip.h"
+#include "num.h"
 
 static t_rc_event	explode(t_map_p map,
 						t_player_p player, double angle, t_img_descriptor_p id);
@@ -45,14 +46,14 @@ static t_rc_event	explode(t_map_p map,
 	t_rc_event	rce;
 
 	rcs = (t_rc_sett){.angle = angle, .flag = {cf_cast}, .start = player->pos,
-		.max = 10};
+		.max = ft_max(map->height, map->width) + 1};
 	rc = rc_throw(map, rcs);
 	rce.collision = rc.pos;
 	rce.start = player->pos;
 	rce.dist = rc.dist * cos(player->angle - angle);
 	rce.offset = rc.offset;
-	// rce.img = tm_get_texture(id, id_texture_e + rc.face);
-	rce.img = tm_get_texture(id, 0 + rc.face);
+	rce.img = tm_get_texture(id, id_texture_e + rc.face);
+	// rce.img = tm_get_texture(id, 0 + rc.face);
 	// rce.img = tm_get_texture(id, id_display);
 	return (rce);
 }
