3a12bbd72bd0efdc75ab74471a05fea55169b4e9
gcros <guilhem.cros.38@gmail.com>
2024-10-30 16:50:58 +0100

    fix mouse

--- srcs/kb_event/kb_mouse_update.c
+++ srcs/kb_event/kb_mouse_update.c
@@ -6,23 +6,23 @@
 /*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 21:39:03 by gcros             #+#    #+#             */
-/*   Updated: 2024/10/30 14:20:02 by gcros            ###   ########.fr       */
+/*   Updated: 2024/10/30 16:26:25 by gcros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 #include "kb_event.h"
 #include "mlx_manip.h"
+#include "defines.h"
 
 static void	update_posy(t_window_p win, t_kb_event_p kbe, int height);
 static void	update_posx(t_window_p win, t_kb_event_p kbe, int width);
 
 void	kb_mouse_update(t_window_p win, t_kb_event_p kbe)
 {
-	int	width;
-	int	height;
+	const int	width = WIN_WIDTH;
+	const int	height = WIN_HEIGHT;
 
-	mlx_get_screen_size(win->mlx_ptr, &width, &height);
 	kbe->lmouse_x = kbe->mouse_x;
 	kbe->lmouse_y = kbe->mouse_y;
 	mlx_mouse_get_pos(win->mlx_ptr, win->win_ptr, &kbe->mouse_x, &kbe->mouse_y);
