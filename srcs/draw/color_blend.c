37980d0cab4fed821aa026fe0e4feb7656ea9f07
gcros <guilhem.cros.38@gmail.com>
2024-11-07 13:43:09 +0100

    opti color blend, fix minmap

--- srcs/draw/color_blend.c
+++ srcs/draw/color_blend.c
@@ -6,7 +6,7 @@
 /*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 18:12:12 by gcros             #+#    #+#             */
-/*   Updated: 2024/11/06 15:14:52 by gcros            ###   ########.fr       */
+/*   Updated: 2024/11/07 13:31:57 by gcros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,14 @@ t_color	color_blend(const t_color c1, const t_color c2)
 	t_color			o;
 	const double    a1 = c1.a *  0.00392156863;
 
+	if (c1.a == 0)
+		return (c1);
+	else if (c1.a == 255)
+	{
+		o = c2;
+		o.a = 255;
+		return (o);
+	}
     o.r = c1.r * (1. - a1) + c2.r * a1;
     o.g = c1.g * (1. - a1) + c2.g * a1;
     o.b = c1.b * (1. - a1) + c2.b * a1;
