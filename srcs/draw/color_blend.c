513e7b0b22ce156c475c56d0651be93a68ddc3ed
ma-gabriel <gabi.eymat@gmail.com>
2024-11-11 13:14:20 +0100

    yeah

--- srcs/draw/color_blend.c
+++ srcs/draw/color_blend.c
@@ -16,7 +16,7 @@
 t_color	color_blend(const t_color c1, const t_color c2)
 {
 	t_color			o;
-	const double	a1 = c1.a * 0.00392156863;
+	double			a1;
 
 	if (c1.a == 0)
 		return (c1);
@@ -26,6 +26,7 @@ t_color	color_blend(const t_color c1, const t_color c2)
 		o.a = 255;
 		return (o);
 	}
+	a1 = c1.a * 0.00392156863;
 	o.r = c1.r * (1. - a1) + c2.r * a1;
 	o.g = c1.g * (1. - a1) + c2.g * a1;
 	o.b = c1.b * (1. - a1) + c2.b * a1;
