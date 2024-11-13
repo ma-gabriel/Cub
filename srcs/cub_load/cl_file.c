82022382344425c1a8a886c8580125485b983452
gcros <guilhem.cros.38@gmail.com>
2024-11-13 20:18:24 +0100

    forgot empty file error

--- srcs/cub_load/cl_file.c
+++ srcs/cub_load/cl_file.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_file.c                                         :+:      :+:    :+:   */
+/*   cl_file.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 06:01:53 by geymat            #+#    #+#             */
-/*   Updated: 2024/10/18 16:49:18 by gcros            ###   ########.fr       */
+/*   Updated: 2024/11/13 20:17:40 by gcros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@
 #include <stdio.h>
 #include "mlx.h"
 #include "mlx_manip.h"
+
+static int	is_file_empty(char **file);
 
 int	read_fd(int fd, char ***file)
 {
@@ -67,6 +69,32 @@ char	**read_file(char *file)
 		close(fd);
 		return (NULL);
 	}
+	if (is_file_empty(res))
+	{
+		ft_putstr_fd(ERR "empty file" NL, 2);
+		ft_strsfree(res);
+		res = NULL;
+	}
 	close(fd);
 	return (res);
+}
+
+static int	is_file_empty(char **file)
+{
+	size_t	i;
+	size_t	j;
+
+	j = 0;
+	while (file[j])
+	{
+		i = 0;
+		while (file[j][i])
+		{
+			if (!(file[j][i] == ' ' || (file[j][i] >= 9 && file[j][i] <= 13)))
+				return (0);
+			i++;
+		}
+		j++;
+	}
+	return (1);
 }
